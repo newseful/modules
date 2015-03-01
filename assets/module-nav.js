81d7e8a9e0f1dd218cc85c87113e46cdeb623a8f
Kevin Zweerink <kevin@kevinzweerink.com>
2015-03-01 10:23:19 -0500

    add navigation system for modules

---
+++ assets/module-nav.js
@@ -0,0 +1,53 @@
+var modulenav = document.createElement('div');
+modulenav.classList.add('newseful-module-nav');
+var title = document.createElement('h1');
+
+var backLink = document.createElement('a');
+backLink.setAttribute('href', '/');
+backLink.innerHTML = '\u2190';
+title.appendChild(backLink);
+
+var list = document.createElement('ul');
+
+var moduleRoutes = [
+{
+	name : 'Annotation Helper',
+	slug : '/annotation_helper/'
+},
+{
+	name : 'Timeline',
+	slug : '/timeline/'
+}
+]
+
+var currentModule;
+
+var urlForSlug = function(s) {
+	return s + 'index.html';
+}
+
+moduleRoutes.map(function(m){
+
+	if (window.location.pathname.match(m.slug)) {
+		currentModule = m;
+		title.innerHTML += 'Modules: '+ m.name;
+	}
+
+	var li = document.createElement('li');
+
+	if (currentModule === m)
+		li.classList.add('current');
+
+	var a = document.createElement('a');
+	a.setAttribute('href', urlForSlug(m.slug))
+	a.innerHTML = m.name;
+
+	li.appendChild(a);
+	list.appendChild(li);
+
+});
+
+modulenav.appendChild(title);
+modulenav.appendChild(list);
+
+document.body.insertBefore(modulenav, document.body.firstChild);
